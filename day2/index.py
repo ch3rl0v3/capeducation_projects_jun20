d11ebfed26820100935f678185aff3a27c889015
ch3rl0v3 <kimaiko33@gmail.com>
2026-07-20 00:25:28 +0500

    Add files via upload

---
+++ day2/index.py
@@ -0,0 +1,18 @@
+name = "Aiko"
+age = 14
+result = 4.5
+is_student = True
+print(name, age, result, is_student)
+
+num = "500"
+num2 = int(num)
+print(num2)
+print(num2 + 100)
+
+stroke = 500
+stroke2 = str(stroke)
+print(stroke2)
+
+float1 = 5
+float2 = float(float1)
+print(float2)
---
+++ day2/index2.py
@@ -0,0 +1,11 @@
+num1 = 10
+num2 = 2
+print(num1 + num2, num1 - num2, num1 * num2, num1 / num2, num1 // num2, num1 % num2, num1 ** num2)
+
+num1 += 5
+num2 -= 1
+print(num1, num2)
+
+text1 = int(input("Enter a number:"))
+result = text1 * 2
+print(result)
